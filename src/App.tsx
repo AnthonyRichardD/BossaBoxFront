291a964487ed827eb7c49a9675ee0fe123d86630
Anthony <106710005+AnthonyRichardD@users.noreply.github.com>
2025-04-13 13:32:40 -0300

    add tailwind configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 function App() {
-
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello</h1>
+      <h1 className="ml-4 flex h-24 border-2 border-gray-300 p-3 text-gray-700 shadow-md">
+        Hello
+      </h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
